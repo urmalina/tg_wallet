eba0c1e24ec3a9763c32ccc54d6d1742abd9b498
urmalina <142245641+urmalina@users.noreply.github.com>
2024-10-08 21:55:06 +0300

    swappimg completely works (ton-jetton, jetton-ton, jetton-jetton)

---
+++ app/jettons.tsx
@@ -0,0 +1,39 @@
+export const jettons  = {
+    "jettons": [
+        { 
+            "address": "0:b113a994b5024a16719f69139328eb759596c38a25f59028b146fecdc3621dfe",
+            "name": "Tether USD",
+            "symbol": "USD₮",
+            "decimals": "6",
+            "image": "https://cache.tonapi.io/imgproxy/T3PB4s7oprNVaJkwqbGg54nexKE0zzKhcrPv8jcWYzU/rs:fill:200:200:1/g:no/aHR0cHM6Ly90ZXRoZXIudG8vaW1hZ2VzL2xvZ29DaXJjbGUucG5n.webp",
+            "description": "Tether Token for Tether USD"
+        
+        },
+        {
+            "address": "0:09f2e59dec406ab26a5259a45d7ff23ef11f3e5c7c21de0b0d2a1cbe52b76b3d",
+            "name": "Hamster Kombat",
+            "symbol": "HMSTR",
+            "decimals": "9",
+            "image": "https://cache.tonapi.io/imgproxy/DhlMJpUMVnrEJfRgoDF-ku6QXuAgdhw4FaPURlbLauw/rs:fill:200:200:1/g:no/aHR0cHM6Ly90b2tlbi5oYW1zdGVya29tYmF0Z2FtZS5pby90b2tlbi9pY29uLnBuZw.webp",
+            "description": "Unleash your inner CEO",
+            "custom_payload_api_uri": "https://proof.hamsterkombatgame.io/jettons/EQAJ8uWd7EBqsmpSWaRdf_I-8R8-XHwh3gsNKhy-UrdrPcUo/"                    
+        },
+        {
+            
+            "address": "0:2f956143c461769579baef2e32cc2d7bc18283f40d20bb03e432cd603ac33ffc",
+            "name": "Notcoin",
+            "symbol": "NOT",
+            "decimals": "9",
+            "image": "https://cache.tonapi.io/imgproxy/4KCMNm34jZLXt0rqeFm4rH-BK4FoK76EVX9r0cCIGDg/rs:fill:200:200:1/g:no/aHR0cHM6Ly9jZG4uam9pbmNvbW11bml0eS54eXovY2xpY2tlci9ub3RfbG9nby5wbmc.webp"
+        
+        },
+        {
+            "address": "0:afc49cb8786f21c87045b19ede78fc6b46c51048513f8e9a6d44060199c1bf0c",
+            "name": "Dogs",
+            "symbol": "DOGS",
+            "decimals": "9",
+            "image": "https://cache.tonapi.io/imgproxy/6Pb0sBFy_AzW6l39EIHGs-Iz4eLbbZUh8AYY_Xq-rcg/rs:fill:200:200:1/g:no/aHR0cHM6Ly9jZG4uZG9ncy5kZXYvZG9ncy5wbmc.webp"
+        
+        }
+    ]
+};
--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,9 @@ interface JettonInfo {
 import { Address, toNano, Sender, SenderArguments } from "@ton/core";
 import { useTonConnectUI } from "@tonconnect/ui-react";
 import { useCallback, useEffect, useState } from "react";
-import { Factory, MAINNET_FACTORY_ADDR, Asset, PoolType, ReadinessStatus } from '@dedust/sdk';
+import { Factory, MAINNET_FACTORY_ADDR, Asset, PoolType, ReadinessStatus, JettonRoot, VaultJetton } from '@dedust/sdk';
 import { TonClient4 } from "@ton/ton";
-
-
+import {jettons} from './jettons';
 
 export default function Home() {
   const [tonConnectUI] = useTonConnectUI();
@@ -36,58 +35,132 @@ export default function Home() {
   const [tonBalance, setTonBalance] = useState<number | null>(null);  
   const [jettonAssets, setJettonAssets] = useState<JettonAsset[]>([]);  
   const [isBalanceShown, setIsBalanceShown] = useState(false); // Новое состояние для показа баланса
+  const [tokenFrom, setTokenFrom] = useState<string>('TON'); // Токен, который отдаем
+  const [tokenTo, setTokenTo] = useState<string>('USDT');   // Токен, который получаем
 
   const tonClient = new TonClient4({ endpoint: "https://mainnet-v4.tonhubapi.com" });
   const factory = tonClient.open(Factory.createFromAddress(MAINNET_FACTORY_ADDR));
+  const [amountToSwap, setAmountToSwap] = useState<string>(''); // Сумма для обмена
+  
+  
+  // Функция для получения address по символу токена
+  function getAddressBySymbol(symbol: string): string {
+    const token = jettons.jettons.find(jetton => jetton.symbol === symbol);
+    return token ? token.address : "";
+  } 
 
   // Функция для свопа токенов
   const handleSwapTokens = async () => {
     try {
-      // Инициализация пула и токенов
-      const tonVault = tonClient.open(await factory.getNativeVault());
-      const SCALE_ADDRESS = Address.parse('EQBlqsm144Dq6SjbPI4jjZvA1hqTIP3CvHovbIfW_t-SCALE');
-      const TON = Asset.native();
-      const SCALE = Asset.jetton(SCALE_ADDRESS);
-      const pool = tonClient.open(await factory.getPool(PoolType.VOLATILE, [TON, SCALE]));
+      const isFromTON = tokenFrom === "TON" 
+      ? true
+      : false;
 
-      // Проверка состояния пула
-      if ((await pool.getReadinessStatus()) !== ReadinessStatus.READY) {
-        throw new Error('Pool (TON, SCALE) does not exist.');
-      }
-
-      // Проверка состояния vault
-      if ((await tonVault.getReadinessStatus()) !== ReadinessStatus.READY) {
-        throw new Error('Vault (TON) does not exist.');
-      }
-
-      const amountIn = toNano('0.1'); // 0.1 TON
-
-      // Инициализация объекта Sender через TonConnect
-      const sender: Sender = {
-        address: Address.parse(tonWalletAddress),
-        send: async (args: SenderArguments) => {
-          await tonConnectUI.sendTransaction({
-            validUntil: Math.floor(Date.now() / 1000) + 60, // Время действия транзакции
-            messages: [
-              {
-                address: args.to.toString(),
-                amount: args.value.toString(),
-                //bounce: args.bounce || false,
-                //stateInit: args.init ? args.init.toBoc().toString("base64") : undefined,
-                payload: args.body ? args.body.toBoc().toString("base64") : undefined
-              }
-            ]
-          });
+      if (isFromTON)
+      {
+        const vault = tonClient.open(await factory.getNativeVault());
+        const FROM_TOKEN = Asset.native();
+        const TO_ADDRESS = Address.parse(getAddressBySymbol(tokenTo));
+        const TO_TOKEN = Asset.jetton(TO_ADDRESS);
+        const pool = tonClient.open(await factory.getPool(PoolType.VOLATILE, [FROM_TOKEN, TO_TOKEN]));        
+        
+        // Проверка состояния пула
+        if ((await pool.getReadinessStatus()) !== ReadinessStatus.READY) {
+          throw new Error(`Pool (TON, ${tokenTo}) does not exist.`);
         }
-      };
+        
+        // Проверка состояния vault
+        if ((await vault.getReadinessStatus()) !== ReadinessStatus.READY) {
+          throw new Error(`Vault ${tokenFrom} does not exist.`);
+        }
 
-      // Выполнение свопа
-      await tonVault.sendSwap(sender, {
-        poolAddress: pool.address,
-        amount: amountIn,
-        gasAmount: toNano("0.25"), // Газ для транзакции
-      });
+        //const amountIn = toNano(amountToSwap); // 0.1 TON
 
+        // Инициализация объекта Sender через TonConnect
+        const sender: Sender = {
+          address: Address.parse(tonWalletAddress),
+          send: async (args: SenderArguments) => {
+            await tonConnectUI.sendTransaction({
+              validUntil: Math.floor(Date.now() / 1000) + 60, // Время действия транзакции
+              messages: [
+                {
+                  address: args.to.toString(),
+                  amount: args.value.toString(),
+                  //bounce: args.bounce || false,
+                  //stateInit: args.init ? args.init.toBoc().toString("base64") : undefined,
+                  payload: args.body ? args.body.toBoc().toString("base64") : undefined
+                }
+              ]
+            });
+          }
+        };
+        //const token = jettons.jettons.find(jetton => jetton.symbol === tokenFrom);
+        //const decimals = token!.decimals;
+        //const decimals = jettons.jettons.find(jetton => jetton.symbol === symbol)
+        const amountIn = toNano(amountToSwap); 
+        //const poolAddress = pool.address;
+
+
+        // Выполнение свопа
+        await vault.sendSwap(sender, {
+          poolAddress: pool.address,
+          amount: amountIn,
+          gasAmount: toNano("0.2"), // Газ для транзакции
+        });
+
+      }
+      else
+      {
+
+        const vault =tonClient.open(await factory.getJettonVault(Address.parse(getAddressBySymbol(tokenFrom))));
+        const FROM_ADDRESS = Address.parse(getAddressBySymbol(tokenFrom));
+        const FROM_TOKEN = Asset.jetton(FROM_ADDRESS);        
+        const TO_TOKEN = tokenTo ==="TON"
+        ? Asset.native()
+        : Asset.jetton(Address.parse(getAddressBySymbol(tokenTo)));
+
+        const pool = tokenTo ==="TON"
+        ?tonClient.open(await factory.getPool(PoolType.VOLATILE, [TO_TOKEN, FROM_TOKEN]))
+        :tonClient.open(await factory.getPool(PoolType.VOLATILE, [FROM_TOKEN, TO_TOKEN])); 
+        //const pool = tonClient.open(await factory.getPool(PoolType.VOLATILE, [FROM_TOKEN, TO_TOKEN]));
+        const jettonRoot = tonClient.open(JettonRoot.createFromAddress(FROM_ADDRESS));
+        
+       // Инициализация объекта Sender через TonConnect
+        const sender: Sender = {
+          address: Address.parse(tonWalletAddress),
+          send: async (args: SenderArguments) => {
+            await tonConnectUI.sendTransaction({
+              validUntil: Math.floor(Date.now() / 1000) + 60, // Время действия транзакции
+              messages: [
+                {
+                  address: args.to.toString(),
+                  amount: args.value.toString(),
+                  //bounce: args.bounce || false,
+                  //stateInit: args.init ? args.init.toBoc().toString("base64") : undefined,
+                  payload: args.body ? args.body.toBoc().toString("base64") : undefined
+                }
+              ]
+            });
+          }
+        };
+        const jettonWallet = tonClient.open(await jettonRoot.getWallet(sender.address!));
+
+        const token = jettons.jettons.find(jetton => jetton.symbol === tokenFrom);
+        const decimals = token!.decimals;
+        //const decimals = jettons.jettons.find(jetton => jetton.symbol === symbol)
+        const amountIn = parseFloat(amountToSwap) * Math.pow(10, parseInt(decimals)); 
+        const poolAddress = pool.address;
+
+        await jettonWallet.sendTransfer(sender, toNano("0.3"), {
+          amount: BigInt(amountIn),
+          destination: vault.address,
+          responseAddress: sender.address, // return gas to user
+          forwardAmount: toNano("0.25"),
+          forwardPayload: VaultJetton.createSwapPayload({poolAddress}),
+        });
+      }
+
+      
       alert("Swap successful!");
 
     } catch (error) {
@@ -96,6 +169,7 @@ export default function Home() {
     }
   };
  
+  
  
   const getRawAddress = async (address: string) => {    
     try {
@@ -231,70 +305,26 @@ export default function Home() {
     return `${tempAddress.slice(0, 4)}...${tempAddress.slice(-4)}`;
   };
 
+  // Обработчик изменения токена исходного свопа
+  const handleTokenFromChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setTokenFrom(e.target.value);
+  };
 
-  
-  // return (
-  //   <main className="flex min-h-screen flex-col items-center justify-center">
-  //     <h1 className="text-4xl font-bold mb-8">TON Wallet</h1>
-  //     {tonWalletAddress ? (
-  //       <div className="flex flex-col items-center">
-  //         <p className="mb-4">Connected: {formatAddress(tonWalletAddress)}</p>
-          
-  //         {/* Кнопка для показа баланса */}
-  //         <button
-  //           onClick={handleShowBalance}
-  //           className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4"
-  //         >
-  //           Show Balance
-  //         </button>
-  
-  //         {/* Показываем баланс только если кнопка была нажата */}
-  //         {isBalanceShown && tonBalance !== null && (
-  //           <div>
-  //             {/* Отображение баланса TON */}
-  //             <p className="mb-4">Balance: {tonBalance} TON</p>
-  
-  //             {/* Отображение жеттонов */}
-  //             <h2 className="text-2xl font-bold mb-4">Jetton Assets</h2>
-  //             {jettonAssets.length > 0? (
-  //               jettonAssets.map((asset, index) => (
-  //                 <p key={index}>
-  //                   {asset.name}: {asset.balance} {asset.symbol}
-  //                 </p>
-  //               ))
-  //             ) : (
-  //               <p>No jetton assets found.</p>
-                
-  //             )}
-  //           </div>
-  //         )}
-  
-  //         <button
-  //           onClick={handleWalletAction}
-  //           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-  //         >
-  //           Disconnect Wallet
-  //         </button>
-  //       </div>
-  //     ) : (
-  //       <button
-  //         onClick={handleWalletAction}
-  //         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-  //       >
-  //         Connect TON Wallet
-  //       </button>
-  //     )}
-  //   </main>
-  // );
+  // Обработчик изменения токена целевого свопа
+  const handleTokenToChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setTokenTo(e.target.value);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-8">TON Wallet</h1>
+
       {tonWalletAddress ? (
         <div className="flex flex-col items-center">
           <p className="mb-4">Connected: {formatAddress(tonWalletAddress)}</p>
           
-          {/* Кнопка для показа баланса */}
-          <button
+           {/* Кнопка для показа баланса */}
+           <button
             onClick={handleShowBalance}
             className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4"
           >
@@ -320,31 +350,126 @@ export default function Home() {
               )}
             </div>
           )}
-  
-          {/* Кнопка для выполнения свопа 5 TON на SCALE */}
+
+          {/* Форма для выбора токенов и суммы */}
+          <div className="mb-4">
+            {/* Выбор токена, который отдаем */}
+            <label className="mr-4">Swap from:</label>
+            <select value={tokenFrom} onChange={handleTokenFromChange} className="p-2 border rounded mr-4">
+              <option value="TON">TON</option>
+              <option value="USD₮">USDT</option>
+              <option value="HMSTR">HMSTR</option>
+              <option value="DOGS">DOGS</option>
+              <option value="NOT">NOT</option>
+            </select>
+
+            {/* Выбор токена, который получаем */}
+            <label className="mr-4">Swap to:</label>
+            <select value={tokenTo} onChange={handleTokenToChange} className="p-2 border rounded">
+              <option value="TON">TON</option>
+              <option value="USD₮">USDT</option>
+              <option value="HMSTR">HMSTR</option>
+              <option value="DOGS">DOGS</option>
+              <option value="NOT">NOT</option>
+            </select>
+          </div>
+
+          {/* Поле для ввода суммы */}
+          <input
+            type="number"
+            value={amountToSwap}
+            onChange={(e) => setAmountToSwap(e.target.value)}
+            placeholder="Enter amount to swap"
+            className="mb-4 p-2 border rounded"
+          />
+
+          {/* Кнопка для выполнения свопа */}
           <button
-            onClick={handleSwapTokens} // Вызов функции свопа токенов
+            onClick={handleSwapTokens} // Вызов функции для перехода на страницу свопа
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
           >
-            Swap 5 TON for SCALE
+            Swap Tokens
           </button>
-  
+
+          {/* Кнопка для отключения кошелька */}
           <button
             onClick={handleWalletAction}
             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
           >
             Disconnect Wallet
           </button>
-        </div>
-      ) : (
-        <button
-          onClick={handleWalletAction}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Connect TON Wallet
-        </button>
-      )}
+          </div>
+          ) : (
+            <button
+              onClick={handleWalletAction}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Connect TON Wallet
+            </button>
+          )}
     </main>
   );
-  
 }
+
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-center">
+//       <h1 className="text-4xl font-bold mb-8">TON Wallet</h1>
+//       {tonWalletAddress ? (
+//         <div className="flex flex-col items-center">
+//           <p className="mb-4">Connected: {formatAddress(tonWalletAddress)}</p>
+          
+//           {/* Кнопка для показа баланса */}
+//           <button
+//             onClick={handleShowBalance}
+//             className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4"
+//           >
+//             Show Balance
+//           </button>
+  
+//           {/* Показываем баланс только если кнопка была нажата */}
+//           {isBalanceShown && tonBalance !== null && (
+//             <div>
+//               {/* Отображение баланса TON */}
+//               <p className="mb-4">Balance: {tonBalance} TON</p>
+  
+//               {/* Отображение жеттонов */}
+//               <h2 className="text-2xl font-bold mb-4">Jetton Assets</h2>
+//               {jettonAssets.length > 0 ? (
+//                 jettonAssets.map((asset, index) => (
+//                   <p key={index}>
+//                     {asset.name}: {asset.balance} {asset.symbol}
+//                   </p>
+//                 ))
+//               ) : (
+//                 <p>No jetton assets found.</p>
+//               )}
+//             </div>
+//           )}
+  
+//           {/* Кнопка для выполнения свопа 5 TON на SCALE */}
+//           <button
+//             onClick={handleSwapTokens} // Вызов функции свопа токенов
+//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+//           >
+//             Swap 5 TON for SCALE
+//           </button>
+  
+//           <button
+//             onClick={handleWalletAction}
+//             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+//           >
+//             Disconnect Wallet
+//           </button>
+//         </div>
+//       ) : (
+//         <button
+//           onClick={handleWalletAction}
+//           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+//         >
+//           Connect TON Wallet
+//         </button>
+//       )}
+//     </main>
+//   );
+  
+// }
